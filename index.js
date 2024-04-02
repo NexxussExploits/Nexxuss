f729e64714e010f097d03ed5b3d886bca9dbbf10
Nexxuss (SniperLyfe) <nexxuss.exploits@gmail.com>
2024-04-02 16:48:39 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// "npm run build" command to build in Render.
+     function App() {
+       return (
+         <div>
+           <h1>Hello, World!</h1>
+         </div>
+       );
+     }
+     
+     export default App;
+     
